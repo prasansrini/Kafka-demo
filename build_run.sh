37db7af30fd671c92465486bf12077a283d5478d
Prasanna <github@prasansrini.com>
2024-07-26 15:59:10 +0200

    Added Docker compose support.

--- build_run.sh
+++ build_run.sh
@@ -2,4 +2,6 @@
 
 mvn clean package || exit
 docker build -t kafka-docker-demo . || exit
-docker run --name kafka-docker-demo -p 8090:8090 kafka-docker-demo || exit
+
+# Enable this if Docker compose is not used.
+#docker run --name kafka-docker-demo -p 8090:8090 kafka-docker-demo || exit
