b33c9143950d04e7647693b178da907bb8162f87
Prasanna Srinivasan <prasanna.srinivasan@mittlogik.se>
2024-07-31 10:54:41 +0200

    Fix script.

--- build_run.sh
+++ build_run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-mvn clean package || exit
+mvn clean install -f ./build/Kafka-demo-aoo/pom.xml || exit
 docker build -t kafka-docker-demo . || exit
+
 if [ $# -eq 0 ]
     then
         docker tag kafka-docker-demo prasansrini29/kafka-docker-demo || exit
