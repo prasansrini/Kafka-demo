28d50b1a48de961fa6185ae7f543688ed653f7c2
Prasanna Srinivasan <prasanna.srinivasan@mittlogik.se>
2024-07-31 10:09:26 +0200

    Added support for pusing Docker image to Docker hub.

--- build_run.sh
+++ build_run.sh
@@ -2,9 +2,9 @@
 
 mvn clean package || exit
 docker build -t kafka-docker-demo . || exit
-docker tag kafka-docker-demo prasansrini29/kafka-docker-demo
-docker login -u prasansrini29
-docker push prasansrini29/kafka-docker-demo
+docker tag kafka-docker-demo prasansrini29/kafka-docker-demo || exit
+docker login -u prasansrini29 || exit
+docker push prasansrini29/kafka-docker-demo || exit
 
 # Enable this if Docker compose is not used.
 #docker run --name kafka-docker-demo -p 8090:8090 kafka-docker-demo || exit
