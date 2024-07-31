89f5ac7e8cff350eb12150222c7f10f6abcbe12d
Prasanna Srinivasan <prasanna.srinivasan@mittlogik.se>
2024-07-31 14:39:30 +0200

    Code cleanup

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,18 @@ EXPOSE 8090
 ENV JAVA_OPTS="-Dspring.profiles.active=local"
 
 ENTRYPOINT ["sh", "-c", "java $JAVA_OPTS -jar /kafka-demo/kafka-demo.jar"]
+
+# First stage: build the application
+# FROM maven:3.8.3-jdk-21 AS build
+# COPY . /kafka-demo
+# WORKDIR /kafka-demo
+# RUN mvn package -DskipTests
+
+# # Second stage: create a slim image
+# FROM openjdk:21-slim
+
+# COPY --from=build kafka-demo/target/OUTPUT.jar /kafka-demo/kafka-demo.jar
+
+# ENV JAVA_OPTS="-Dspring.profiles.active=local"
+
+# ENTRYPOINT ["sh", "-c", "java $JAVA_OPTS -jar /kafka-demo/kafka-demo.jar"]
--- build_run.sh
+++ build_run.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-mvn clean install -f ./build/Kafka-demo-app/pom.xml || exit
-docker build -t kafka-docker-demo ./build/Kafka-demo-app || exit
-
-if [ $# -eq 0 ]
+if [ "$1" == "" ]
     then
-        docker tag kafka-docker-demo prasansrini29/kafka-docker-demo || exit
-        docker login -u prasansrini29 || exit
-        docker push prasansrini29/kafka-docker-demo || exit
+    echo "Usage: ./build_run.sh <DOCKER_HUB_PASSWORD>"
+    exit
 fi
+
+mvn clean install || exit
+docker build -t kafka-docker-demo . || exit
+
+docker tag kafka-docker-demo prasansrini29/kafka-docker-demo || exit
+docker login -u prasansrini29 -p $1 || exit
+docker push prasansrini29/kafka-docker-demo || exit
 
 # Enable this if Docker compose is not used.
 #docker run --name kafka-docker-demo -p 8090:8090 kafka-docker-demo || exit
