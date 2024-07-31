71de4ecdc76c50e8f382804036d73dcdfd71227e
Prasanna Srinivasan <prasanna.srinivasan@mittlogik.se>
2024-07-31 10:35:41 +0200

    Fix script for automated build.

--- build_run.sh
+++ build_run.sh
@@ -2,9 +2,12 @@
 
 mvn clean package || exit
 docker build -t kafka-docker-demo . || exit
-docker tag kafka-docker-demo prasansrini29/kafka-docker-demo || exit
-docker login -u prasansrini29 || exit
-docker push prasansrini29/kafka-docker-demo || exit
+if [ $# -eq 0 ]
+    then
+        docker tag kafka-docker-demo prasansrini29/kafka-docker-demo || exit
+        docker login -u prasansrini29 || exit
+        docker push prasansrini29/kafka-docker-demo || exit
+fi
 
 # Enable this if Docker compose is not used.
 #docker run --name kafka-docker-demo -p 8090:8090 kafka-docker-demo || exit
